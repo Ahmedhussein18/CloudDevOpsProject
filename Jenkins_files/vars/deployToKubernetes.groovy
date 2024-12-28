a85e44ecc3a701e669828069530cef2fe3f73474
Ahmedhussein18 <77368474+Ahmedhussein18@users.noreply.github.com>
2024-12-28 22:49:43 +0200

    Update deployToKubernetes.groovy

--- Jenkins_files/vars/deployToKubernetes.groovy
+++ Jenkins_files/vars/deployToKubernetes.groovy
@@ -3,7 +3,16 @@ def call(Map envVars) {
     echo 'Updating deployment file...'
     sh """
     sed -i 's|image: .*|image:${envVars.dockerUsername}/${envVars.imageName}:${envVars.buildNumber}|g' deployment.yaml
+   
+    echo 'Pushing New Deployment file to Github..'
+    git config user.name "${envVars.githubUsername}"
+    git config user.email "${envVars.githubEmail}"
+    git remote set-url origin ${envVars.gitRepo}
+    git checkout ${envVars.branch}
+    git pull origin ${envVars.branch}
+    git add .
+    git commit -m "Automated commit by Jenkins" || echo "No changes to commit"
+    git push origin ${envVars.branch}
     """
-
 
 }
