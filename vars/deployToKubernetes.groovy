ff2dcf51091db9a3a6fd7ee646fd296b3a5ffe6c
Ahmedhussein18 <77368474+Ahmedhussein18@users.noreply.github.com>
2024-12-28 18:53:37 +0200

    update

--- vars/deployToKubernetes.groovy
+++ vars/deployToKubernetes.groovy
@@ -2,16 +2,8 @@ def call(Map envVars) {
     // Update Deployment YAML
     echo 'Updating deployment file...'
     sh """
-    sed -i 's|<IMAGE_PLACEHOLDER>|${envVars.dockerUsername}/${envVars.imageName}:${envVars.buildNumber}|g' deployment.yaml
+    sed -i 's|image: .*|image:${envVars.dockerUsername}/${envVars.imageName}:${envVars.buildNumber}|g' deployment.yaml
     """
 
-    // Deploy to Kubernetes
-    echo 'Deploying to Kubernetes...'
-    withCredentials([file(credentialsId: envVars.kubeconfig, variable: 'KUBE_CONFIG')]) {
-        sh '''
-        export KUBECONFIG=$KUBE_CONFIG
-        kubectl apply -f deployment.yaml
-        kubectl apply -f service.yaml
-        '''
-    }
+
 }
