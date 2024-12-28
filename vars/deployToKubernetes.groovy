4d5b850f75768a7cb13b61b6be8e5478a6a47852
Ahmedhussein18 <77368474+Ahmedhussein18@users.noreply.github.com>
2024-12-28 22:45:09 +0200

    Delete vars directory

--- vars/deployToKubernetes.groovy
+++
@@ -1,9 +0,0 @@
-def call(Map envVars) {
-    // Update Deployment YAML
-    echo 'Updating deployment file...'
-    sh """
-    sed -i 's|image: .*|image:${envVars.dockerUsername}/${envVars.imageName}:${envVars.buildNumber}|g' deployment.yaml
-    """
-
-
-}
